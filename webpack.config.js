73035d24621fa724fb33e86b090114ab3a72fb9f
Trevor Lemon <duhlemman@gmail.com>
2019-02-10 01:42:56 -0800

    Files cleaned on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
 
 const HASH_LENGTH = 5;
+const OUTPUT_DIR = 'example/dist';
 const stats = {
   chunks: false,
   colors: true,
@@ -52,13 +54,18 @@ module.exports = {
     },
   },
   output: {
-    path: resolve(__dirname, 'example/dist'),
+    path: resolve(__dirname, OUTPUT_DIR),
     // assigns the hashed name to the file
     filename: `[name]_[chunkhash:${ HASH_LENGTH }].js`,
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info => resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
   },
   plugins: [
+    new CleanWebpackPlugin([
+      `${ OUTPUT_DIR }/*.*`,
+    ], {
+      
+    }),
     /**
      * Gives more control of how bundles are hashed
      */
